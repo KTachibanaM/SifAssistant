fdc24b5c8abf151b95593c259b69dc18ffd8535d
KTachibanaM <whj19931115@gmail.com>
2015-07-21 18:02:16 +0800

    Add add-account

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,36 @@
 angular.module('sif-assistant.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
+// With the new view caching in Ionic, Controllers are only called
+// when they are recreated or on app start, instead of every page change.
+// To listen for when this page is active (for example, to refresh data),
+// listen for the $ionicView.enter event:
+//$scope.$on('$ionicView.enter', function(e) {
+//})
+
+.controller('AppCtrl', function($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('templates/add-account.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.addAccountModal = modal;
+    });
+
+    $scope.addAccount = function (alias, region, level, exp) {
+        console.log(alias, region, level, exp);
+        $scope.closeAddAccount();
+    };
+
+    $scope.openAddAccount = function () {
+        $scope.addAccountModal.show();
+    };
+
+    $scope.closeAddAccount = function () {
+        $scope.addAccountModal.hide();
+    };
+
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
 })
 
 .controller('AccountsCtrl', function ($scope) {
