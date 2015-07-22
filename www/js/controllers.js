b92c60c67254b11a706b53df19e43ed37568851a
KTachibanaM <whj19931115@gmail.com>
2015-07-22 19:22:03 +0800

    Clear LP

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -109,6 +109,13 @@ angular.module('sif-assistant.controllers', ['sif-assistant.services'])
                     text: "Cancel"
                 },
                 {
+                    text: "Clear",
+                    onTap: function (e) {
+                        e.preventDefault();
+                        $scope.updateAccountData.updatedLp = 0;
+                    }
+                },
+                {
                     text: '<b>Save</b>',
                     type: 'button-positive',
                     onTap: function () {
