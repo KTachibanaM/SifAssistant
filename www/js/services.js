78c30f188b3d0a4ed34ec60a97572da7c0d8a5b9
KTachibanaM <whj19931115@gmail.com>
2015-07-22 20:45:31 +0800

    max lp

--- www/js/services.js
+++ www/js/services.js
@@ -14,14 +14,24 @@ angular.module('sif-assistant.services', [])
     }
 }])
 
-.factory('Accounts', ['$localStorage', function ($localStorage) {
+.factory('Calculators', function () {
+    return {
+        getMaxLpByLevel: function (level) {
+            return 25 + Math.floor(Math.min(level, 300) / 2) + Math.floor(Math.max((level - 300), 0) / 3);
+        }
+    }
+})
+
+.factory('Accounts', ['$localStorage', 'Calculators', function ($localStorage, Calculators) {
     const ACCOUNTS_KEY = "accounts";
     return {
+        KEY_FOR_ALIAS: "alias",
         KEY_FOR_LEVEL: "level",
         KEY_FOR_EXP: "exp",
         KEY_FOR_LP: "lp",
         KEY_FOR_LOVECA: "loveca",
         KEY_FOR_BONUS: "has_claimed_bonus",
+        KEY_FOR_MAX_LP: "max_lp",
         set: function (accounts) {
             $localStorage.set(ACCOUNTS_KEY, accounts)
         },
@@ -33,11 +43,13 @@ angular.module('sif-assistant.services', [])
             var current_aliases = current_accounts.map(function (item) {
                 return item.alias
             });
-            return current_aliases.indexOf(account.alias);
+            return current_aliases.indexOf(account[this.KEY_FOR_ALIAS]);
         },
         addAccount: function (new_account) {
             if (new_account !== undefined) {
-                new_account.has_claimed_bonus = false;
+                new_account[this.KEY_FOR_BONUS] = false;
+                var level = new_account[this.KEY_FOR_LEVEL];
+                new_account[this.KEY_FOR_MAX_LP] = Calculators.getMaxLpByLevel(level);
                 var current_accounts = this.get();
                 current_accounts.push(new_account);
                 this.set(current_accounts);
@@ -60,6 +72,8 @@ angular.module('sif-assistant.services', [])
                 var current_accounts = this.get();
                 var index = this.getAccountIndex(account);
                 current_accounts[index][key] = newData;
+                var level = current_accounts[index][this.KEY_FOR_LEVEL];
+                current_accounts[index][this.KEY_FOR_MAX_LP] = Calculators.getMaxLpByLevel(level);
                 this.set(current_accounts);
                 return true;
             }
