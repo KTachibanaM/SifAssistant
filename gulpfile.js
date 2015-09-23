ecfc8f8307cc5ca3d9356f17b9641a9dfca89869
KTachibanaM <whj19931115@gmail.com>
2015-09-23 03:00:58 -0500

    Remove del task

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,12 @@ gulp.task('watch', function () {
     gulp.watch(src.js, ['jsx-js']);
 });
 
-gulp.task('clean-js', function () {
-    return del([dist.js])
-});
-
-gulp.task('jsx-js', ['clean-js'], function () {
+gulp.task('jsx-js', function () {
     return gulp.src(src.js)
         .pipe(babel())
+        .on('error', function (e) {
+            console.error('jsx-js task error', e);
+            this.emit('end');
+        })
         .pipe(gulp.dest(dist.js));
 });
