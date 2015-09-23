9161c36b572b4310aaa6f9a54de6332dfda6cc0a
KTachibanaM <whj19931115@gmail.com>
2015-09-23 01:43:57 -0500

    Clean up gulp and npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sh = require('shelljs');
 
 var src = {
     js: ['./www/js/**/*.js', './www/js/**/*.jsx']
