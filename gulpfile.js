625287c44af7cd7edd9f0f0db7a9fb295e81b63b
KTachibanaM <whj19931115@gmail.com>
2015-09-23 02:24:48 -0500

    Add clean-js task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+var gulp = require('gulp'),
+    del = require('del'),
+    babel = require('gulp-babel');
 
 var src = {
     js: ['./www/js/**/*.js', './www/js/**/*.jsx']
@@ -9,13 +10,17 @@ var dist = {
     js: './www/dist'
 };
 
-gulp.task('default', ['babel']);
+gulp.task('default', ['jsx-js']);
 
 gulp.task('watch', function () {
-    gulp.watch(src.js, ['babel']);
+    gulp.watch(src.js, ['jsx-js']);
 });
 
-gulp.task('babel', function () {
+gulp.task('clean-js', function () {
+    return del([dist.js])
+});
+
+gulp.task('jsx-js', ['clean-js'], function () {
     return gulp.src(src.js)
         .pipe(babel())
         .pipe(gulp.dest(dist.js));
