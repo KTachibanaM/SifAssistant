85631a37ba7eacffe8b0f015fea45270077897d5
KTachibanaM <whj19931115@gmail.com>
2015-09-24 13:27:13 -0500

    Scrolling

--- www/js/App.jsx
+++ www/js/App.jsx
@@ -17,6 +17,38 @@ var App = React.createClass({
                     lp: 70, // Changing per second
                     next_lp: 6 * 60 * 1000, // Changing per second
                     love_gems: 0
+                },
+                {
+                    alias: "test3",
+                    level: 5,
+                    exp: 23,
+                    lp: 4, // Changing per second
+                    next_lp: 6 * 60 * 1000, // Changing per second
+                    love_gems: 233
+                },
+                {
+                    alias: "test4",
+                    level: 100,
+                    exp: 2333,
+                    lp: 70, // Changing per second
+                    next_lp: 6 * 60 * 1000, // Changing per second
+                    love_gems: 0
+                },
+                {
+                    alias: "test5",
+                    level: 5,
+                    exp: 23,
+                    lp: 4, // Changing per second
+                    next_lp: 6 * 60 * 1000, // Changing per second
+                    love_gems: 233
+                },
+                {
+                    alias: "test6",
+                    level: 100,
+                    exp: 2333,
+                    lp: 70, // Changing per second
+                    next_lp: 6 * 60 * 1000, // Changing per second
+                    love_gems: 0
                 }
             ]
         }
