47880f2b2e2a89ba81de5e99d223b112c6c9f358
KTachibanaM <whj19931115@gmail.com>
2015-09-24 22:57:40 -0500

    Add gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     babel = require('gulp-babel');
 
 var src = {
@@ -18,9 +19,6 @@ gulp.task('watch', function () {
 gulp.task('jsx-js', function () {
     return gulp.src(src.js)
         .pipe(babel())
-        .on('error', function (e) {
-            console.error('jsx-js task error', e);
-            this.emit('end');
-        })
+        .on('error', gutil.log)
         .pipe(gulp.dest(dist.js));
 });
