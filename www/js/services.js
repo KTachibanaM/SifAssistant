919f5a4f7a93d4f00875db1037684c50c3017dd0
KTachibanaM <whj19931115@gmail.com>
2016-08-28 13:56:08 -0500

    AppUpdate service (not going to use it now)

--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,80 @@
+/**
+ * Compares two software version numbers (e.g. "1.7.1" or "1.2b").
+ *
+ * This function was born in http://stackoverflow.com/a/6832721.
+ *
+ * @param {string} v1 The first version to be compared.
+ * @param {string} v2 The second version to be compared.
+ * @param {object} [options] Optional flags that affect comparison behavior:
+ * <ul>
+ *     <li>
+ *         <tt>lexicographical: true</tt> compares each part of the version strings lexicographically instead of
+ *         naturally; this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than
+ *         "1.2".
+ *     </li>
+ *     <li>
+ *         <tt>zeroExtend: true</tt> changes the result if one version string has less parts than the other. In
+ *         this case the shorter string will be padded with "zero" parts instead of being considered smaller.
+ *     </li>
+ * </ul>
+ * @returns {number|NaN}
+ * <ul>
+ *    <li>0 if the versions are equal</li>
+ *    <li>a negative integer iff v1 < v2</li>
+ *    <li>a positive integer iff v1 > v2</li>
+ *    <li>NaN if either version string is in the wrong format</li>
+ * </ul>
+ *
+ * @copyright by Jon Papaioannou (["john", "papaioannou"].join(".") + "@gmail.com")
+ * @license This function is in the public domain. Do what you want with it, no strings attached.
+ */
+function version_compare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
+
 String.prototype.hashCode = function () {
     var hash = 0, i, chr, len;
     if (this.length == 0) return hash;
@@ -662,7 +739,7 @@ angular.module('sif-assistant.services', [])
                         callback(ionic.Platform.platform())
                     })
                 } else {
-                    callback('browser')
+                    callback('web')
                 }
             },
             getPlatformVersion: function (callback) {
@@ -916,6 +993,63 @@ angular.module('sif-assistant.services', [])
                     if (present) {
                         // if daily event check is present, cancel
                         NativeSchedule.cancel("_daily_event_check");
+                    }
+                })
+            }
+        }
+    })
+
+    .factory('AppUpdate', function (Platform, $http) {
+        return {
+            isUpdateAvailable: function (callback) {
+                Platform.getPlatform(function (platform) {
+                    if (platform === 'android' || platform === 'ios') {
+                        // get latest release version from GitHub if on mobile
+                        $http.get('https://api.github.com/repos/KTachibanaM/SifAssistant/releases').then(function (payload) {
+                            // check for version diff
+                            var releases = payload.data;
+                            var latest_release_version = releases[0]['tag_name'];
+                            Platform.getAppVersion(function (app_version) {
+                                if (version_compare(app_version, latest_release_version) < 0) {
+                                    callback(true)
+                                } else {
+                                    callback(false)
+                                }
+                            })
+                        }, function (error) {
+                            callback(false)
+                        })
+                    } else if (platform === 'web') {
+                        // probably debugging on web, no need to check
+                        callback(false)
+                    } else {
+                        callback(false)
+                    }
+                });
+            },
+            getUpdateLink: function (callback) {
+                Platform.getPlatform(function (platform) {
+                    if (platform === 'android') {
+                        // get latest release from GitHub if on Android
+                        $http.get('https://api.github.com/repos/KTachibanaM/SifAssistant/releases').then(function (payload) {
+                            var releases = payload.data;
+                            var android_assets = releases[0]['assets'].filter(function (asset) {
+                                return asset['content_type'] === 'application/vnd.android.package-archive'
+                            });
+                            if (android_assets.length === 1) {
+                                callback(android_assets[0]['browser_download_url'])
+                            } else {
+                                callback('https://github.com/KTachibanaM/SifAssistant/releases')
+                            }
+                        }, function (error) {
+                            callback('https://github.com/KTachibanaM/SifAssistant/releases')
+                        })
+                    } else if (platform === 'ios') {
+                        callback('https://itunes.apple.com/us/app/idol-manager/id1030763734')
+                    } else if (platform === 'web') {
+                        callback('https://github.com/KTachibanaM/SifAssistant')
+                    } else {
+                        callback('https://github.com/KTachibanaM/SifAssistant')
                     }
                 })
             }
